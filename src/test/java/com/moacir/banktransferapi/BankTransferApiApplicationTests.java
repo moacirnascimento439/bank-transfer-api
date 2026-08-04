434e5b0456173c0161a8e3bcab6bf821960293e1
moacirnascimento439 <moacirnascimento1999@gmail.com>
2026-08-04 19:11:42 +0000

    chore: cria esqueleto do projeto Spring Boot 4.1 com Java 21

---
+++ src/test/java/com/moacir/banktransferapi/BankTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moacir.banktransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
